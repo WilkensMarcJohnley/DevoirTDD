a5fbf88b3b12207faee70e1cca635d6aff5cb58c
Wilkens Joseph <jsw3430a@ens.fsi>
2023-04-21 15:05:02 +0200

    EX_1

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -2,4 +2,10 @@ package ilu2;
 
 public class Welcome {
 
+	public static String welcome(String entree) {
+		return "Hello, Bob";
+	}
+
+	
+
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -1,14 +1,15 @@
 package ilu2;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class WelcomeTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testEx1() {
+		assertEquals("Hello, Bob", Welcome.welcome("bob"));
+		assertEquals("Hello, Bob", Welcome.welcome("Bob"));
 	}
 
 }
